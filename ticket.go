f2a9dc3f5ca6de5cd9cd9f03abc14ec3d0e532ca
Jason Kingsbury <jason@relva.co.uk>
2016-08-24 19:16:56 +0100

    much better worker management, improvements to Destroy handling

--- ticket.go
+++ ticket.go
@@ -1,5 +1,7 @@
 package gpool
 
+import "fmt"
+
 // ticket request types
 type tReq int
 
@@ -40,5 +42,109 @@ func newTicket(r tReq, data interface{}) ticket {
 		t:    r,
 		data: data,
 		r:    make(chan error, 1),
+	}
+}
+
+func (p *pool) processTicketRequest(t ticket) {
+	switch t.t {
+	// New Job request
+	case tReqJobStartCallback, tReqJobStopCallback, tReqJobQueue:
+		if p.state > OK {
+			t.r <- ErrClosedPool
+			return
+		}
+		u := uuid()
+		if u == "" {
+			t.r <- fmt.Errorf("unable to generate uuid")
+			return
+		}
+
+		p.putQueueState(&JobState{ID: u, j: t.data.(Job), t: t})
+		if t.t == tReqJobQueue {
+			t.r <- nil
+		}
+	case tReqBatchJobQueue:
+		if p.state > OK {
+			t.r <- ErrClosedPool
+			return
+		}
+		jobs := t.data.([]Job)
+		for _, j := range jobs {
+			u := uuid()
+			if u == "" {
+				t.r <- fmt.Errorf("unable to generate uuid")
+				return
+			}
+			p.putQueueState(&JobState{ID: u, j: j, t: t})
+		}
+		t.r <- nil
+	// Pool close request
+	case tReqClose:
+		if p.state >= Closed {
+			t.r <- ErrClosedPool
+			return
+		}
+		if p.state == OK && p.intent < wantStop {
+			p.intent = wantStop
+		}
+		t.r <- nil
+		return
+	// Pool kill request
+	case tReqKill:
+		if p.state > Closed {
+			t.r <- ErrKilled
+			return
+		}
+		if p.intent < wantKill {
+			p.intent = wantKill
+		}
+		t.r <- nil
+		return
+	// Pool wait request
+	case tReqWait:
+		// If done resolve ticket instantly
+		if p.state == Done {
+			p.acknowledge(p.err, t)
+			return
+		}
+		p.pendWait = append(p.pendWait, t)
+	case tReqGrow, tReqShrink:
+		i := t.data.(int)
+		if t.t == tReqShrink {
+			i = -i
+		}
+		if p.state != OK {
+			t.r <- ErrClosedPool
+			return
+		}
+		target := adjust(len(p.workers), i)
+		if target > 0 {
+			p.resolveWorkers(target)
+			t.r <- nil
+		}
+		t.r <- ErrWorkerCount
+	case tReqResize:
+		i := t.data.(int)
+		if p.state != OK {
+			t.r <- ErrClosedPool
+			return
+		}
+		if i == 0 {
+			t.r <- ErrWorkerCount
+			return
+		}
+		p.resolveWorkers(i)
+		t.r <- nil
+	case tReqGetError:
+		t.r <- p.err
+	case tReqDestroy:
+		if p.state == OK && p.intent == OK {
+			p.intent = wantStop
+		}
+		p.pendDestroy = append(p.pendDestroy, t)
+	case tReqStat:
+		t.r <- &returnPayload{data: p.stat()}
+	default:
+		panic(fmt.Sprintf("unexpected ticket type %d", t.t))
 	}
 }
